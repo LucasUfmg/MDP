06d767355f9c7192a532c3e04a3e8569a92f9cdc
Lucas Rodrigues <fisicaufmglucas@hotmail.com>
2026-07-07 15:12:24 -0300

    right url for download

--- R/data_catalog.R
+++ R/data_catalog.R
@@ -1,10 +1,10 @@
 data_catalog <- list(
 
   focos =
-    "https://github.com/LucasUfmg/MDP/releases/tag/download/v_0.1.0/focos_package_2016_2024.gpkg",
+    "https://github.com/LucasUfmg/MDP/releases/download/v_0.1.0/focos_package_2016_2024.gpkg",
 
   deforestation =
-    "https://github.com/LucasUfmg/MDP/releases/tag/download/v_0.1.0/deter_package.gpkg"
+    "https://github.com/LucasUfmg/MDP/releases/download/v_0.1.0/deter_package.gpkg"
 
 )
 
