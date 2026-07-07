1bda075b29408af7d898fd09b3cd362a5599101a
Lucas Rodrigues <fisicaufmglucas@hotmail.com>
2026-07-07 14:20:24 -0300

    change url deter_package

--- R/data_catalog.R
+++ R/data_catalog.R
@@ -4,6 +4,7 @@ data_catalog <- list(
     "https://github.com/LucasUfmg/MDP/releases/tag/v_0.1.0/focos_package_2016_2024.gpkg",
 
   deforestation =
-    "https://github.com/LucasUfmg/MDP/releases/tag/v_0.1.0/deter_package.gpkg"
+    "https://raw.githubusercontent.com/LucasUfmg/MDP/releases/tag/v_0.1.0/deter_package.gpkg"
 
 )
+
